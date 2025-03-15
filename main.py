1df0ed212e1e379c8ae51f6a30f018c9f5adcdff
Stepani2YT <stepapugavka@gmail.com>
2025-03-15 20:37:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+while True:
+	file = input('file name: ')
+
+	# Load a model
+	model = YOLO("yolo11m-seg.pt")  # load a custom model
+
+	# Predict with the model
+	results = model(file)  # predict on an image
+
+	# Access the results
+	#for result in results:
+	#    xy = result.masks.xy  # mask in polygon format
+	#    xyn = result.masks.xyn  # normalized
+	#    masks = result.masks.data  # mask in matrix format (num_objects x H x W)
+	input()
