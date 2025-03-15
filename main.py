b2533c1162b8d1f3292cf46d3c087cc7257e61f6
Stepani2YT <stepapugavka@gmail.com>
2025-03-15 20:55:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from ultralytics import YOLO
+from wget import download
+try:
+	download('https://drive.usercontent.google.com/download?id=1fwpJVLt8sL6s13wqLPg4gAPbf8qAo0Xd&export=download&authuser=0&confirm=t&uuid=b1abd8de-dd81-4d36-a64f-5a5788dbc383&at=AEz70l4LfEdeqw1zTnVVP8SD8miI%3A1742060987035','yolo11m-seg.pt')
+except:
+	print('')
 while True:
-	file = input('file name: ')
+	file = input('\nfile name: ')
 
 	# Load a model
 	model = YOLO("yolo11m-seg.pt")  # load a custom model
